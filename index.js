9d0e127faa2a7d75f2c7407adac8ed9d1353c1c0
Kanji Bhanderi <kanji@unoindia.co>
2024-02-18 21:19:21 +0530

    test commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req, res) => {
-res.send('welcome in home test CI/CD! updated1')
+res.send('welcome in home test CI/CD! updated3')
 })
 
 app.get('/test',(req, res) => {
-    res.send('tested /test route updated1')
+    res.send('tested /test route updated3')
     })
 
 app.listen(3000,() => {
